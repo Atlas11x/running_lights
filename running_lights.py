47c0122e33c699cc6c72e98f5ae5b901481a284d
Atlas11x <sashat022007@gmail.com>
2022-09-24 17:12:19 +0600

    first working version! (fixed bugs)

--- running_lights.py
+++ running_lights.py
@@ -3,4 +3,31 @@ import os
 import time
 
 #init arr
-array = []
+array = [0, 0, 0, 0]
+
+#init varriables
+delay = int(1)
+
+#loop
+while True:
+	for i in range(len(array) + 1):
+
+
+		#reset list
+		array[0] = 0
+		array[1] = 0
+		array[2] = 0
+		array[3] = 0
+
+
+		#light on
+		array[i - 1] = 1
+
+		#output
+		print (str(array[0]) + " " + str(array[1]) + " " + str(array[2]) + " " + str(array[3]))
+
+		#delay
+		time.sleep(delay)
+
+		#display reset
+		os.system("clear")
